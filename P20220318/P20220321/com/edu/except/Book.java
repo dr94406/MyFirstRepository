65860343a47f13d2eba2a2379e87e7806008f98e
Admin <Admin@MIN>
2022-03-21 22:17:28 +0900

    20220321 커밋

--- P20220318/P20220321/com/edu/except/Book.java
+++ P20220318/P20220321/com/edu/except/Book.java
@@ -13,7 +13,7 @@ public class Book {
 
 	public void showInfo() {
 		System.out.println("=======================================================");
-		System.out.println("책제목: " + this.title + "저자는 " + this.author + "가격은" + this.price);
+		System.out.println(" 책제목 : " + this.title + " 저자는 " + this.author + " 가격은 " + this.price);
 		System.out.println("=======================================================");
 	}
 
--- P20220318/P20220321/com/edu/except/LibraryExe.java
+++ P20220318/P20220321/com/edu/except/LibraryExe.java
@@ -14,24 +14,24 @@ public class LibraryExe {
 			int menu = 0;
 
 			while (true) {
-				showMessage("메뉴 선택: 1.책 정보입력, 2.리스트 보기, 3.종료");
+				showMessage(" 메뉴 선택: 1.책 정보입력, 2.리스트 보기, 3.종료");
 				try {
 					menu = scn.nextInt();
 					break;
 				} catch (InputMismatchException e) {
-					System.out.println("숫자를 입력하세요.");
+					System.out.println(" 숫자를 입력하세요.");
 					scn.next(); // a
 				}
 			}
 
 			if (menu == 1) {
-				showMessage("책 제목을 입력하세요>>>");
+				showMessage(" 책 제목을 입력하세요.>>>");
 				String title = scn.next();
-				showMessage("저자를 입력하세요>>>");
+				showMessage(" 저자를 입력하세요.>>>");
 				String author = scn.next();
 				int price = 0;
 				while (true) {
-					showMessage("가격을 입력하세요>>>");
+					showMessage(" 가격을 입력하세요.>>>");
 					price = scn.nextInt();
 					break;
 				}
@@ -42,7 +42,7 @@ public class LibraryExe {
 						break;
 					}
 				}
-				showMessage("저장완료.");
+				showMessage(" 저장완료.");
 			} else if (menu == 2) {
 				for (int i = 0; i < library.length; i++) {
 					if (library[i] != null) {
@@ -50,19 +50,19 @@ public class LibraryExe {
 					}
 				}
 			} else if (menu == 3) {
-				showMessage("프로그램을 종료합니다.");
+				showMessage(" 프로그램을 종료합니다.");
 				break;
 			}
 		} // end of while()
 
-		showMessage("==end of program===");
+		showMessage(" ==end of program===");
 	}
 
 	private static void showMessage(String str) {
 		for (int i = 0; i < str.length(); i++) {
 			System.out.print(str.charAt(i));
 			try {
-				Thread.sleep(300);// 300>0.3초
+				Thread.sleep(1);// 300>0.3초
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
