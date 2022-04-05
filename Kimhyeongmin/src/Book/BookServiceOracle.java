891c84b79830f29e1a2cf206aeda0a96954d20cf
Admin <Admin@MIN>
2022-04-05 23:12:36 +0900

    2022-04-05 개인프로젝트

--- Kimhyeongmin/src/Book/BookServiceOracle.java
+++ Kimhyeongmin/src/Book/BookServiceOracle.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookServiceOracle extends DAO implements BookService {
-
 	// 입력처리
 	@Override
 	public void insertBook(Book book) {
@@ -19,20 +18,15 @@ public class BookServiceOracle extends DAO implements BookService {
 			psmt.setString(3, book.getAuthor());
 			psmt.setString(4, book.getPublisher());
 			psmt.setInt(5, book.getPrice());
-//			psmt.setString(6, book.getRental());
-
 			int r = psmt.executeUpdate(); // 실행된 건수를 반환해줍니다.
 			System.out.println(r + "건 입력되었습니다.");
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			// 정상실행, 예외발생 => 반드시 실행코드.
 			disconnect();
 		}
-
 	}
-
 	// 한 건 조회. (책번호로)
 	@Override
 	public Book getBook(int aoa) { // 매개변수 타입
@@ -59,22 +53,16 @@ public class BookServiceOracle extends DAO implements BookService {
 		}
 		return book;
 	}
-
 	// 전체조회
 	@Override
 	public List<Book> bookList() {
 		List<Book> list = new ArrayList<Book>(); // 조회된 결과값을 담기위한 컬렉션.
-
 		conn = getConnect(); // select * from book_info order by book_id;
 		try {
 			psmt = conn.prepareStatement("select * from book_info where rental not in ('Y') order by book_id");
 			rs = psmt.executeQuery(); // 실행건수만큼 반복자.
 			while (rs.next()) { // 반복자를 통해 요소를 가지고 올 수 있는지 체크. 있으면 하나 가지고 옴.
 				Book book = new Book();
-
-				// 출력되는지 확인
-//				System.out.println(rs.getString("title"));
-
 				book.setBookId(rs.getInt("book_Id")); // 값을 읽어와서 지정하겠습니다.
 				book.setTitle(rs.getString("title"));
 				book.setAuthor(rs.getString("author"));
@@ -88,10 +76,8 @@ public class BookServiceOracle extends DAO implements BookService {
 		} finally {
 			disconnect();
 		}
-
 		return list;
 	}
-
 	@Override // 수정처리
 	public void modifyBook(Book book) {
 		conn = getConnect();
@@ -104,7 +90,6 @@ public class BookServiceOracle extends DAO implements BookService {
 			psmt.setString(3, book.getPublisher());
 			psmt.setInt(4, book.getPrice());
 			psmt.setInt(5, book.getBookId());
-			
 			int r = psmt.executeUpdate(); // 실행된 건수를 반환해줍니다.
 			System.out.println(r + "건 수정되었습니다.");
 		} catch (SQLException e) {
@@ -114,7 +99,6 @@ public class BookServiceOracle extends DAO implements BookService {
 			disconnect();
 		}
 	}
-
 	// 삭제처리
 	@Override
 	public void removeBook(int glo) {
@@ -131,7 +115,6 @@ public class BookServiceOracle extends DAO implements BookService {
 			disconnect();
 		}
 	}
-
 	@Override // 이름으로로 조회 기능.
 	public List<Book> searchBook(String gro) {
 		List<Book> Books = new ArrayList<Book>();
@@ -159,12 +142,9 @@ public class BookServiceOracle extends DAO implements BookService {
 		}
 		return Books;
 	}
-
 	@Override
 	public void saveToFile() { // 안해도됨.
-
 	}
-
 	@Override // 대여기능 
 	public void rentalBook(Book book) {
 		conn = getConnect();
@@ -182,5 +162,4 @@ public class BookServiceOracle extends DAO implements BookService {
 			disconnect();
 		}
 	}
-
 }
